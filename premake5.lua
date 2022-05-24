79d48a10053bed74ce1d605d0575f14e80db3ddf
Simon Rodriguez <kosua20@gmail.com>
2022-05-24 20:07:21 +0200

    Add initial project.

---
+++ premake5.lua
@@ -0,0 +1,60 @@
+
+
+workspace("eXporter112")
+	-- Configuration.
+	configurations({ "Release", "Dev"})
+	location("build")
+	targetdir ("build/%{prj.name}/%{cfg.longname}")
+	debugdir ("build/%{prj.name}/%{cfg.longname}")
+	architecture("x64")
+	systemversion("latest")
+
+	-- Configuration specific settings.
+	filter("configurations:Release")
+		defines({ "NDEBUG" })
+		optimize("On")
+
+	filter("configurations:Dev")
+		defines({ "DEBUG" })
+		symbols("On")
+
+	filter({})
+	startproject("eXporter112")
+
+	project("eXporter112")
+		kind("ConsoleApp")
+
+		language("C++")
+		cppdialect("C++17")
+
+		-- Compiler flags
+		filter("toolset:not msc*")
+			buildoptions({ "-Wall", "-Wextra" })
+		filter("toolset:msc*")
+			buildoptions({ "-W3"})
+		filter({})
+		-- visual studio filters
+		filter("action:vs*")
+			defines({ "_CRT_SECURE_NO_WARNINGS" })  
+		filter({})
+
+		-- Common include dirs
+		-- System headers are used to support angled brackets in Xcode.
+		includedirs({"src/"})
+		sysincludedirs({ "libs/", "src/libs" })
+
+		-- common files
+		files({"src/main.cpp", "src/core/**", "src/libs/**.hpp", "src/libs/*/*.cpp", "src/libs/**.h", "src/libs/*/*.c", "premake5.lua"})
+		removefiles({"**.DS_STORE", "**.thumbs"})
+
+		
+
+newaction {
+   trigger     = "clean",
+   description = "Clean the build directory",
+   execute     = function ()
+      print("Cleaning...")
+      os.rmdir("./build")
+      print("Done.")
+   end
+}
