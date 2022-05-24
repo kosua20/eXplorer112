cf615f6a38f6d434e06f901fe1e7fc82877f6a42
Simon Rodriguez <kosua20@gmail.com>
2022-05-24 22:42:05 +0200

    Tool to list and parse all DFF files.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
-#include <stdio>
+#include "core/Log.hpp"
+#include "core/System.hpp"
+#include "core/DFFParser.hpp"
 
-int main( int argc, char** argv )
+int main(int argc, const char** argv)
 {
+	if(argc < 2){
+		return 1;
+	}
+	const fs::path rootPath(argv[1]);
+	const fs::path modelsPath = rootPath / "models";
+	const fs::path modelsPath = rootPath / "models";
+	const fs::path modelsPath = rootPath / "models";
+
+	for (const fs::directory_entry& file : fs::recursive_directory_iterator(modelsPath)) {
+		const fs::path& path = file.path();
+		if(path.extension() == ".dff"){
+
+			Log::info("%s", path.c_str());
+
+			Dff::Context context;
+			Dff::parse(path, context);
+
+		}
+	}
+
 	return 0;
 }
